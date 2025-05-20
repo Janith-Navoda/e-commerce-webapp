866cb8118145f6b2467cdfe5002bd638c523c361
Janith-Navoda <156767620+Janith-Navoda@users.noreply.github.com>
2025-05-21 04:34:22 +0530

    Added the login and register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import { Toaster } from "sonner";
-
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    
     <BrowserRouter>
-    <Toaster position="top-right" />
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
         <Route>{/*Admin Layout */}</Route>
       </Routes>

22e27dd8fd26dfbd5a6f261dbdd2e39ffac5f525
Janith-Navoda <156767620+Janith-Navoda@users.noreply.github.com>
2025-05-21 13:14:50 +0530

    Added frontend of profile and myorderspage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { Toaster } from "sonner";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route index element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
         <Route>{/*Admin Layout */}</Route>
       </Routes>

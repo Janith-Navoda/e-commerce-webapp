ca0b835f96b4c4458a84ae104406acd85246ae65
Janith-Navoda <156767620+Janith-Navoda@users.noreply.github.com>
2025-05-26 13:39:09 +0530

    Added the checkout file

--- frontend/src/components/Products/SortOptions.jsx
+++ frontend/src/components/Products/SortOptions.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const SortOptions = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const handleSortChange = (e) => {
     const sortBy = e.target.value;
-    searchParams.set("sortBy, sortBy");
+    searchParams.set("sortBy", sortBy);
     setSearchParams(searchParams);
   };
   return (
--- frontend/src/pages/CollectionPage.jsx
+++ frontend/src/pages/CollectionPage.jsx
@@ -35,7 +35,7 @@ const CollectionPage = () => {
   return () => {
     document.removeEventListener("mousedown", handleClickOutside);
   };
-}, [isSidebarOpen]);
+}, []);
 
 
   useEffect(() => {

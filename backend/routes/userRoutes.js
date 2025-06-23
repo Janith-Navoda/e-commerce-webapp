13f2dd69908a4301935974c6fbeaa466bda3d9ac
Janith-Navoda <156767620+Janith-Navoda@users.noreply.github.com>
2025-06-23 22:23:40 +0530

    added the register logic

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,7 +12,36 @@ router.post("/register", async (req, res) => {
 
   try {
     // Registration logic
-    res.send({ name, email, password });
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "User already existes" });
+
+    user = new User({ name, email, password });
+    await user.save();
+
+    // Create JWT (jason web token) Payload
+    const payload = { user: { id: user._id, role: user.role } };
+
+    // Sign and return the token along with user data
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+
+        //send the user and token in response
+        res.status(201).json({
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+          },
+          token,
+        });
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(500).send("Server error");

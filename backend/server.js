f5ce817962b6dc079d5c5a1656f621605a73042c
Janith-Navoda <156767620+Janith-Navoda@users.noreply.github.com>
2025-07-01 22:34:18 +0530

    completed the checkout Routes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const { connect } = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
+const checkoutRoutes = require("./routes/checkoutRoutes");
 
 const app = express();
 app.use(express.json());
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/checkout", checkoutRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
